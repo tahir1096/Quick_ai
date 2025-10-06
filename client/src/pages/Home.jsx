7e2d0da38a244867262e447263b9613baa39efcc
Muhammad Tahir <mtahirkaam@gmail.com>
2025-10-06 23:57:03 +0500

    home-page-complete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
 import { Hero } from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
+import { Plan } from '../components/Plan'
+import Footer from '../components/Footer'
 
 export const Home = () => {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <AiTools/>
+      <Testimonial/>
+      <Plan/>
+      <Footer/>
     </>
   )
 }
