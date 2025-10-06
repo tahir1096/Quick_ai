4bbe5beca968a266ae6139fa6d3fabe7feaee7ae
Muhammad Tahir <mtahirkaam@gmail.com>
2025-10-06 21:56:14 +0500

    hero-section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
+import { Hero } from '../components/Hero'
 
 export const Home = () => {
   return (
-    <div>
+    <>
       <Navbar/>
-    </div>
+      <Hero/>
+    </>
   )
 }
